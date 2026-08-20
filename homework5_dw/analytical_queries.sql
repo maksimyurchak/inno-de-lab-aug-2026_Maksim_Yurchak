9f66bd95235d9607165be86eebd42178009ff341
Maksim Yurchak <maksimyurchak12@gmail.com>
2026-08-20 17:56:27 +0300

    Add corrected homework 5

--- homework5_dw/analytical_queries.sql
+++ homework5_dw/analytical_queries.sql
@@ -7,17 +7,20 @@ FROM Fact_sales AS f
 	WHERE d.year = 2026 
 	AND d.month = 'July';
 
+
 -- Calculate revenue for vet specialised in MRI for 2025
 SELECT 
     SUM(f.line_total) AS total_revenue
 FROM Fact_sales AS f
-	JOIN dim_vet AS v ON f.vet_sk = v.vet_sk
-	JOIN dim_date AS d ON f.date_sk = d.date_sk
-	WHERE d.year = 2025
-	AND v.vet_specialisation = 'MRI';
+JOIN dim_vet AS v ON f.vet_sk = v.vet_sk
+JOIN dim_specialisation AS s ON v.specialisation_sk = s.specialisation_sk
+JOIN dim_date d ON f.date_sk = d.date_sk
+WHERE s.specialisation_name = 'MRI'
+  AND d.year = 2025;
+
 
 --Select min and max price of an appointment in July 2026
-WITH AppointmentPrices AS (
+WITH appointment_totals AS (
     SELECT 
         f.bill_sk,
         SUM(f.line_total) AS total_appointment_price
@@ -30,4 +33,17 @@ WITH AppointmentPrices AS (
 SELECT 
     MIN(total_appointment_price) AS min_appointment_price,
     MAX(total_appointment_price) AS max_appointment_price
-FROM AppointmentPrices;
+FROM appointment_totals;
+
+
+--Select the most profitable day of the week in July 2026
+SELECT 
+    d.day_of_week,
+    SUM(f.line_total) AS total_revenue
+FROM Fact_sales AS f
+    JOIN dim_date AS d ON f.date_sk = d.date_sk
+    WHERE d.year = 2026 
+    AND d.month = 'July' 
+    GROUP BY d.day_of_week
+    ORDER BY total_revenue DESC
+    LIMIT 1;
